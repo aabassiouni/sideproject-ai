7ab1c56b33371e5ac759e646423d5f0ff04a4bff
aabassiouni <aabassiouni@hotmail.com>
2023-09-06 08:12:01 +0300

    styling signin and signup pages

--- src/app/signin/[[...sign-in]]/page.tsx
+++ src/app/signin/[[...sign-in]]/page.tsx
@@ -4,7 +4,8 @@ export const runtime = "edge";
 export default function SignInPage() {
 	console.log("Sign in page")
 	return (
-		<div className="flex justify-center h-screen w-screen bg-slate-200 items-center">
+		<div className="flex flex-col gap-5 justify-center h-screen w-screen bg-slate-200 items-center">
+			<h1 className="font-necto font-bold text-2xl">sideproject.ai</h1>
 			<SignIn redirectUrl={"/dashboard"}/>
 		</div>
 	);
--- src/app/signup/[[...sign-up]]/page.tsx
+++ src/app/signup/[[...sign-up]]/page.tsx
@@ -4,7 +4,8 @@ export const runtime = "edge";
 
 export default function Page() {
 	return (
-		<div className="flex justify-center items-center">
+		<div className="flex flex-col gap-5 justify-center h-screen w-screen bg-slate-200 items-center">
+			<h1 className="font-necto font-bold text-2xl">sideproject.ai</h1>
 			<SignUp afterSignInUrl={"/dashboard"} />
 		</div>
 	);
