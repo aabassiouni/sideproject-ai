ebc96c94d53ad94b132b8e3cc912409099618461
aabassiouni <aabassiouni@hotmail.com>
2023-09-13 13:08:08 +0300

    add loading state to generation and signin page

---
+++ src/app/dashboard/[generation]/loading.tsx
@@ -0,0 +1,45 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { ArrowLeftCircle } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+import { Skeleton } from '@/components/ui/skeleton'
+import { Separator } from '@/components/ui/separator'
+function Loading() {
+    return (
+        <div className="flex flex-col items-center justify-center gap-2 px-4">
+            <div className="mt-10 flex flex-col items-center sm:flex-row">
+                <Link className="hidden sm:block" href={'/dashboard'}>
+                    <Button variant={'ghost'} className="m-4  rounded-xl">
+                        <ArrowLeftCircle size={24} />
+                    </Button>
+                </Link>
+                <Card className="flex h-24  w-full flex-col p-2 sm:w-[550px]">
+                    <CardContent className="flex h-full items-center gap-4 self-center">
+                        <Skeleton className="h-4 w-24" />
+                    </CardContent>
+                </Card>
+            </div>
+            <Card className="relative mt-2 h-full sm:w-[850px]">
+                <CardHeader>
+                    <CardTitle>Generated Text</CardTitle>
+                </CardHeader>
+                <div className="flex h-full grow items-center justify-center">
+                    <CardContent className="w-full">
+                        <div className=" h-full w-full space-y-2 rounded-md  bg-slate-100 p-2 text-lg  sm:p-10">
+                            <Skeleton className="h-4 w-24" />
+                            <Separator />
+                            <Skeleton className="h-24 w-full" />
+                            <Skeleton className="h-24 w-full" />
+                            <Skeleton className="h-24 w-full" />
+                            <Skeleton className="h-24 w-full" />
+                            <Skeleton className="h-24 w-full" />
+                        </div>
+                    </CardContent>
+                </div>
+            </Card>
+        </div>
+    )
+}
+
+export default Loading
---
+++ src/app/signin/[[...sign-in]]/loading.tsx
@@ -0,0 +1,13 @@
+import { Skeleton } from '@/components/ui/skeleton'
+import React from 'react'
+
+function Loading() {
+    return (
+        <div className="flex h-screen w-screen flex-col items-center justify-center gap-5 bg-slate-200">
+            <h1 className="font-necto text-2xl font-bold">sideproject.ai</h1>
+            <Skeleton className="h-1/4 w-1/4" />
+        </div>
+    )
+}
+
+export default Loading
