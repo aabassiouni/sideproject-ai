15d9021b682ebde612b76752702a804802baf954
aabassiouni <aabassiouni@hotmail.com>
2023-09-13 12:59:51 +0300

    update middleware to ignore landing

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
 
+
 export default authMiddleware({
   publicRoutes: ["/", "/signin", "/api/webhook/stripe", "/api/webhook/clerk"],
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/dashboard", "/dashboard/(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/dashboard", "/dashboard/(.*)"],
 };
+
