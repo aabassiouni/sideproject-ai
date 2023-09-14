930e0ef23ffacac92b2f5be49d628eed2e3f8c4a
aabassiouni <aabassiouni@hotmail.com>
2023-09-14 06:56:07 +0300

    add robots and sitemap, update middleware for twt card

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 
 export default authMiddleware({
-  publicRoutes: ["/", "/signin","/opengraph-image", "/api/webhook/stripe", "/api/webhook/clerk"],
+  publicRoutes: ["/", "/signin","/opengraph-image","/twitter-image", "/api/webhook/stripe", "/api/webhook/clerk"],
 });
 
 export const config = {
