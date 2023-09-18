6d45e3500659477653cd080044e5a8512a97994e
aabassiouni <aabassiouni@hotmail.com>
2023-09-18 07:21:05 +0300

    add empty contact page

---
+++ src/app/dashboard/contact/page.tsx
@@ -0,0 +1,27 @@
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
+import React from 'react'
+
+function ContactPage() {
+    return (
+        // <div className="flex flex-col justify-center">
+        //     <div className="my-10">
+        //         <h1 className="text-center text-4xl font-bold">Contact</h1>
+        //         <p className="text-center text-xl">Select the amount of credits you would like to purchase.</p>
+        //     </div>
+        //     <div className="flex flex-1 flex-wrap items-center justify-center gap-10">
+        //         <Card className="mx-3 flex min-h-[350px] w-full flex-col border-2 border-blue-400 sm:mx-0 sm:w-64">
+        //             <CardHeader>
+        //                 <CardTitle className="mx-auto text-4xl">Free</CardTitle>
+        //             </CardHeader>
+
+        //             <CardContent className="text-center">
+        //                 sideproject.ai is currently in beta. During this time, all credits are free.
+        //             </CardContent>
+        //         </Card>
+        //     </div>
+        // </div>
+        <h1></h1>
+    )
+}
+
+export default ContactPage
