38259fd16a15360bb5ccf2dd348127fd00980b35
aabassiouni <aabassiouni@hotmail.com>
2023-09-23 18:21:07 +0300

    update middleware to ignore landing page

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,8 @@ import { authMiddleware } from "@clerk/nextjs";
 
 
 export default authMiddleware({
-  publicRoutes: ["/", "/signin","/opengraph-image","/twitter-image", "/api/webhook/stripe", "/api/webhook/clerk"],
+  publicRoutes: [ "/signin","/opengraph-image","/twitter-image", "/api/webhook/stripe", "/api/webhook/clerk"],
+  ignoredRoutes: ["/"]
 });
 
 export const config = {
