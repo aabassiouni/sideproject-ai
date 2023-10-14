4a616826c2e52b4b4e817ccdb34c34913a3c073f
aabassiouni <aabassiouni@hotmail.com>
2023-10-14 21:26:56 +0300

    change heading color for signin and signup page

--- src/app/(dashboard)/signin/[[...sign-in]]/page.tsx
+++ src/app/(dashboard)/signin/[[...sign-in]]/page.tsx
@@ -4,7 +4,7 @@ export const runtime = "edge";
 export default function SignInPage() {
 	return (
 		<div className="flex flex-col gap-5 justify-center h-screen w-screen bg-slate-200 items-center">
-			<h1 className="font-necto font-bold text-2xl">sideprojectAI</h1>
+			<h1 className="font-necto font-bold text-2xl text-slate-900">sideprojectAI</h1>
 			<SignIn redirectUrl={"/dashboard"}/>
 		</div>
 	);
