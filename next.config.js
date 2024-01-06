1748993f2ca963fd0ceb761af330d79f263ff47f
Ali Bassiouni <aabassiouni@hotmail.com>
2024-01-06 16:25:45 +0200

    bump versions

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: true,
-    },
-    // reactStrictMode: true,
-    modularizeImports: {
-        'lucide-react': {
-            transform: 'lucide-react/dist/esm/icons/{{ kebabCase member }}',
-            // skipDefaultConversion: true,
-            preventFullImport: true,
-        },
-    },
+
 }
 
 module.exports = nextConfig
--- src/components/buttons/OnboardingSubmitButton.tsx
+++ src/components/buttons/OnboardingSubmitButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import { experimental_useFormStatus as useFormStatus } from 'react-dom'
+import { useFormStatus } from 'react-dom'
 import { Button } from '../ui/button'
 import { Loader2 } from 'lucide-react'
 
@@ -8,7 +8,7 @@ function OnboardingSubmitButton() {
     const { pending } = useFormStatus()
 
     return (
-        <Button className='w-20' type="submit" aria-disabled={pending}>
+        <Button className="w-20" type="submit" aria-disabled={pending}>
             {pending ? <Loader2 className="h-6 w-6 animate-spin" /> : 'Submit'}
         </Button>
     )
