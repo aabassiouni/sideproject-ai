8bdc9ccb15c55c34ca9b33994541c3a41e5c9c35
Ali Bassiouni <aabassiouni@hotmail.com>
2024-04-05 06:00:34 +0200

    change errors table schema

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -3,9 +3,13 @@ import * as dotenv from "dotenv";
 
 dotenv.config({ path: ".env.local" });
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set");
-}
+// if (!process.env.DATABASE_URL) {
+//   throw new Error("DATABASE_URL is not set");
+// }
+// console.log(process.env.DATABASE_URL);
+// console.log(process.env.DATABASE_HOST);
+// console.log(process.env.DATABASE_USERNAME);
+// console.log(process.env.DATABASE_PASSWORD);
 
 export default {
   schema: "./src/schema.ts",
@@ -13,6 +17,11 @@ export default {
   driver: "mysql2",
   strict: false,
   dbCredentials: {
-    uri: process.env.DATABASE_URL,
+    // host: process.env.DATABASE_HOST,
+    // username: process.env.DATABASE_USERNAME,
+    // password: process.env.DATABASE_PASSWORD,
+    // database: "sideproject_ai",
+    
+    uri: process.env.DATABASE_URL!,
   },
 } satisfies Config;
