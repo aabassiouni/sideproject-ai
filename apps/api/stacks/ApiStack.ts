178e923a4c21eb8277b03280fa6f72e6911a1dfb
Ali Bassiouni <aabassiouni@hotmail.com>
2024-04-06 22:39:25 +0200

    add custom domain to prod stage

--- apps/api/stacks/ApiStack.ts
+++ apps/api/stacks/ApiStack.ts
@@ -1,10 +1,17 @@
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
 import { Api, Config, StackContext } from "sst/constructs";
 
-export function ApiStack({ stack }: StackContext) {
+export function ApiStack({ app, stack }: StackContext) {
   const OPENAI_API_KEY = new Config.Secret(stack, "OPENAI_API_KEY");
   const CLERK_SECRET_KEY = new Config.Secret(stack, "CLERK_SECRET_KEY");
   const DATABASE_URL = new Config.Secret(stack, "DATABASE_URL");
   const DISCORD_WEBHOOK_URL = new Config.Secret(stack, "DISCORD_WEBHOOK_URL");
+
+  if (!process.env.AWS_CUSTOM_DOMAIN || !process.env.AWS_CERTIFICATE_ARN) {
+    throw new Error(
+      "Please set the AWS_CUSTOM_DOMAIN and AWS_CERTIFICATE_ARN environment variables."
+    );
+  }
 
   const api = new Api(stack, "Api", {
     defaults: {
@@ -19,6 +26,22 @@ export function ApiStack({ stack }: StackContext) {
         ],
       },
     },
+    customDomain: app.stage === "prod" ? {
+      domainName: process.env.AWS_CUSTOM_DOMAIN,
+      isExternalDomain: true,
+      cdk: {
+        certificate: Certificate.fromCertificateArn(
+          stack,
+          "MyCert",
+          process.env.AWS_CERTIFICATE_ARN
+        ),
+      },
+    } : undefined,
+    // cors: {
+    //   // allowMethods: ["ANY"],
+    //   // allowHeaders: ["*"],
+    //   allowOrigins: ["https://usesideprojectai.com"],
+    // },
     authorizers: {
       clerk: {
         type: "jwt",
