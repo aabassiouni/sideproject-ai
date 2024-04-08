6e043cd7e0abc714d5974684b67dc323d460bb64
Ali Bassiouni <aabassiouni@hotmail.com>
2024-04-08 05:19:43 +0200

    throw error on missing db url

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,14 +1,18 @@
-import type { Config } from "drizzle-kit";
 import * as dotenv from "dotenv";
+import type { Config } from "drizzle-kit";
 
 dotenv.config({ path: ".env.local" });
 
+if (!process.env.DATABASE_URL) {
+	throw new Error("No database URL");
+}
+
 export default {
-  schema: "./src/schema.ts",
-  out: "./drizzle/migrations",
-  driver: "mysql2",
-  strict: false,
-  dbCredentials: {
-    uri: process.env.DATABASE_URL!,
-  },
+	schema: "./src/schema.ts",
+	out: "./drizzle/migrations",
+	driver: "mysql2",
+	strict: false,
+	dbCredentials: {
+		uri: process.env.DATABASE_URL,
+	},
 } satisfies Config;
