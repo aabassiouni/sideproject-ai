3d099373443f655313ea2208b8cca16fb6df1e6e
Ali Bassiouni <aabassiouni@hotmail.com>
2024-04-09 19:24:57 +0200

    use http through vercel pg pkg

--- apps/web/src/lib/db/db.ts
+++ apps/web/src/lib/db/db.ts
@@ -1,11 +1,12 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-
 import { errors, generations, users } from "@sideproject-ai/db";
+import { createPool } from "@vercel/postgres";
+import { drizzle } from "drizzle-orm/vercel-postgres";
 
 const connectionString = process.env.DATABASE_URL!;
 
-const client = postgres(connectionString, { prepare: false });
+const client = createPool({
+  connectionString: connectionString,
+});
 
 export const db = drizzle(client, {
   schema: {
