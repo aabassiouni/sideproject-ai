206e37b044ad1d79c014a51647dfce19cd20fb0e
Ali Bassiouni <aabassiouni@hotmail.com>
2024-04-12 04:55:24 +0200

    remove log

--- apps/web/src/lib/actions.ts
+++ apps/web/src/lib/actions.ts
@@ -4,7 +4,6 @@ import { revalidatePath } from "next/cache";
 import { deleteGeneration } from "./db";
 
 export async function deleteGenerationAction(generationID: string) {
-  console.log("deleting generation with id:", generationID);
   await deleteGeneration(generationID);
   revalidatePath("/dashboard");
 }
