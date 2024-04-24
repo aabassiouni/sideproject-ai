eb7eb516f017fb756858e592e3f8444f3082a5cb
Ali Bassiouni <aabassiouni@hotmail.com>
2024-04-24 19:37:18 +0200

    Delete prettier.config.js

--- apps/web/prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  trailingComma: "es5",
-  tabWidth: 4,
-  semi: false,
-  singleQuote: true,
-  printWidth: 120,
-  tailwindConfig: "./tailwind.config.js",
-  plugins: [require("prettier-plugin-tailwindcss")],
-};
