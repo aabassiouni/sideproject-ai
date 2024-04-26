a941ba3e5ba0764746fe02898740c0079c98bba7
Ali Bassiouni <aabassiouni@hotmail.com>
2024-04-26 18:54:59 +0300

    fix empty state for no repos (#4)

--- apps/web/src/app/(dashboard)/dashboard/profile/page.tsx
+++ apps/web/src/app/(dashboard)/dashboard/profile/page.tsx
@@ -3,7 +3,7 @@ import { RedirectToSignIn, SignedIn, SignedOut, UserProfile } from "@clerk/nextj
 async function ProfilePage() {
   return (
     <>
-      <div className="flex items-center justify-center bg-slate-200 p-4 dark:bg-gray-900">
+      <div className="flex min-h-screen justify-center bg-slate-200 p-4 dark:bg-gray-900">
         <SignedIn>
           <div className="leading-none">
             <UserProfile />
